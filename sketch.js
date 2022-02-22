eb6aac56cd3d8a2cff8941783606689d49f83c93
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-02-22 16:18:07 -0300

    22/02/2022

--- sketch.js
+++ sketch.js
@@ -2,10 +2,14 @@ var trex, trexCorrendo;
 var borda;
 var solo,chao;
 var vridro;
+var claudinei;
+var argodao;
+
 
 function preload(){
  trexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
  chao = loadImage("ground2.png");
+ argodao = loadImage ("cloud.png");
 }
 
 function setup(){
@@ -40,5 +44,17 @@ function draw(){
 }
 
 function nuvens(){
+if (frameCount%60===0){
+claudinei = createSprite (600, 100, 40, 10);
+claudinei.addImage (argodao);
+claudinei.scale = random(0.5,1);
+claudinei.y = Math.round(random(1,100));
+claudinei.velocityX = -3;
+claudinei.lifetime = 250;
+claudinei.depth = trex.depth;
+trex.depth +=1;
+
+}
+
 
 }
