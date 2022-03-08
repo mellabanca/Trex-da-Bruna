3dd431863330dc59caa9c437ee67c3ea222cd28a
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-03-08 16:20:45 -0300

    08/03/2022

--- sketch.js
+++ sketch.js
@@ -4,12 +4,24 @@ var solo,chao;
 var vridro;
 var claudinei;
 var argodao;
-
+var juliette1, juliette2, juliette3, juliette4, juliette5, juliette6;
+var contador;
+var algodoes;
+var espinhos;
+var START = 1;
+var GAMEOVER = 0;
+var estado = START;
 
 function preload(){
  trexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
  chao = loadImage("ground2.png");
  argodao = loadImage ("cloud.png");
+ juliette1 = loadImage ("obstacle1.png");
+ juliette2 = loadImage ("obstacle2.png");
+ juliette3 = loadImage ("obstacle3.png");
+ juliette4 = loadImage ("obstacle4.png");
+ juliette5 = loadImage ("obstacle5.png");
+ juliette6 = loadImage ("obstacle6.png");
 }
 
 function setup(){
@@ -25,11 +37,32 @@ function setup(){
  vridro.visible = false;
  //var aleatorio = Math.round(random(1,100));
  //console.log(aleatorio);
+ contador = 0;
+ algodoes = new Group();
+ espinhos = new Group();
 }
 
 function draw(){
- background(190);
- solo.velocityX = -2;
+    background(190);
+if(estado === START){
+    if(contador > 100){
+        background(150);
+    }
+    if(contador > 200){
+       background(130);
+   }
+
+   solo.velocityX = -2;
+
+
+
+
+} else if(estado === GAMEOVER){
+   solo.velocityX = 0;
+
+
+}
+
  if (solo.x <0){
  solo.x = solo.width/2;
 }
@@ -40,7 +73,10 @@ function draw(){
  trex.velocityY = trex.velocityY + 1;
  trex.collide(vridro);
  nuvens();
+ mamacos();
  drawSprites();
+ text(contador, 550, 50);
+ contador += Math.round(frameCount/60);
 }
 
 function nuvens(){
@@ -53,7 +89,41 @@ claudinei.velocityX = -3;
 claudinei.lifetime = 250;
 claudinei.depth = trex.depth;
 trex.depth +=1;
+algodoes.add(claudinei);
+}
 
+
+}
+function mamacos (){
+if (frameCount%60===0){
+var carmen = createSprite(600, 165, 10, 40);
+carmen.velocityX = -6;
+var paodequeijo = Math.round(random(1,6));
+switch (paodequeijo) {
+    case 1: carmen.addImage(juliette1);
+        
+        break;
+        case 2: carmen.addImage(juliette2);
+        
+        break;
+        case 3: carmen.addImage(juliette3);
+        
+        break;
+        case 4: carmen.addImage(juliette4);
+        
+        break;
+        case 5: carmen.addImage(juliette5);
+        
+        break;
+        case 6: carmen.addImage(juliette6);
+        
+        break;
+    default:
+        break;
+}
+carmen.scale = 0.5;
+carmen.lifetime = 300;
+espinhos.add(carmen);
 }
 
 
